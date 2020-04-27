a781e22c42ad902a95bd34f0927a65a5be4dbeed
Somnath <somnath@Somnaths-MacBook-Air.local>
2020-04-27 12:36:43 +0530

    Updates

--- precache-manifest.6558763373fad4519030b14da123462d.js
+++ precache-manifest.6558763373fad4519030b14da123462d.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "55133973a3127514f0e9b0cdccfdd3a5",
+    "revision": "ae915a53e15213c2d19dda9da61f330b",
     "url": "/Crio-Launch-March-2020-prerakr/index.html"
   },
   {
-    "revision": "8acbadfbbd99425f2830",
+    "revision": "aa55b9b81612ba4705f4",
     "url": "/Crio-Launch-March-2020-prerakr/static/css/2.57ddf7ad.chunk.css"
   },
   {
-    "revision": "a841267d8f79665fbae7",
+    "revision": "16a37e0255edbce1b302",
     "url": "/Crio-Launch-March-2020-prerakr/static/css/main.5db1380d.chunk.css"
   },
   {
-    "revision": "8acbadfbbd99425f2830",
-    "url": "/Crio-Launch-March-2020-prerakr/static/js/2.ed70f288.chunk.js"
+    "revision": "aa55b9b81612ba4705f4",
+    "url": "/Crio-Launch-March-2020-prerakr/static/js/2.28730628.chunk.js"
   },
   {
     "revision": "d466ab9648f013dea081956fc7c904f3",
-    "url": "/Crio-Launch-March-2020-prerakr/static/js/2.ed70f288.chunk.js.LICENSE.txt"
+    "url": "/Crio-Launch-March-2020-prerakr/static/js/2.28730628.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a841267d8f79665fbae7",
-    "url": "/Crio-Launch-March-2020-prerakr/static/js/main.42b08814.chunk.js"
+    "revision": "16a37e0255edbce1b302",
+    "url": "/Crio-Launch-March-2020-prerakr/static/js/main.4e960503.chunk.js"
   },
   {
-    "revision": "307ffa2c1dce97a81688",
-    "url": "/Crio-Launch-March-2020-prerakr/static/js/runtime-main.6589e2f6.js"
+    "revision": "8be22a944b84b25a553e",
+    "url": "/Crio-Launch-March-2020-prerakr/static/js/runtime-main.ad204a99.js"
   },
   {
     "revision": "c9536e0497f939475da3110091e1a10d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Crio-Launch-March-2020-prerakr/precache-manifest.88fddd8758a6a0090c170c2e620012b7.js"
+  "/Crio-Launch-March-2020-prerakr/precache-manifest.6558763373fad4519030b14da123462d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Crio-Launch-March-2020-prerakr/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
